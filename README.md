26c502620872384dc5c83b4ffc40c4e5f90bf4f1
apetzx <apetz.inc@hotmail.com>
2022-08-01 04:27:28 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-oytaut-j2ocji
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-oytaut-j2ocji)
---
+++ src/app/products.ts
@@ -0,0 +1,43 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: string;
+  description: string;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'Samsung A32',
+    price: '1260',
+    description:
+      'O Samsung Galaxy A32 é um smartphone Android de bom nível, ótimo para fotos, que pode satisfazer até o mais exigente dos usuários. Sobre as características deste Samsung Galaxy A32 na verdade não falta nada.',
+  },
+  {
+    id: 2,
+    name: 'Motorola G60',
+    price: '1460',
+    description:
+      'O Motorola Moto G60 é um smartphone Android avançado e abrangente em todos os pontos de vista com algumas características excelentes. As funcionalidades oferecidas pelo Motorola Moto G60 são muitas e inovadoras.',
+  },
+  {
+    id: 3,
+    name: 'Iphone 11',
+    price: '3230',
+    description:
+      'O celular premium da Apple sucede o iPhone XR com vasta opção de cores, iOS 13 e tela com notch. Entre os destaques estão o sistema de câmera dupla similar ao Galaxy S10E, que agrega um sensor adicional de 12 megapixels com lente ultra wide, e a ficha técnica avançada com o recém-apresentado Apple A13 Bionic.',
+  },
+  {
+    id: 4,
+    name: 'Poco M4 Pro',
+    price: '1565',
+    description:
+      'O Poco M4 Pro 5G é um smartphone Android completo, que não tem muito a invejar aos mais avançados dispositivos. Surpreendente é sua tela Touchscreen de 6.6 polegadas, que coloca esse Poco no topo de sua categoria.',
+  },
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
